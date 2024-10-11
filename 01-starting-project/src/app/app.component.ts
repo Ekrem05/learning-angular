2ed0e274899669ccc886b4cb3a16fb9d5eb842a3
Ekrem Beytula <78847462+Ekrem05@users.noreply.github.com>
2024-10-11 15:35:18 +0300

    First angular component

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'first-angular-app';
-}
+export class AppComponent {}
