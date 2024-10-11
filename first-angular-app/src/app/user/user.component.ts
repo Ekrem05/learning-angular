d7b410a5573496907f4aa799d01d3fa177c21898
Ekrem Beytula <78847462+Ekrem05@users.noreply.github.com>
2024-10-11 17:08:18 +0300

    Dynamic content

--- first-angular-app/src/app/user/user.component.ts
+++ first-angular-app/src/app/user/user.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy_users';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-
+  selectedUser = DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)];
 }
