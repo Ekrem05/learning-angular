8b913115da964562918eb1f5dc273776e1fea114
Ekrem Beytula <78847462+Ekrem05@users.noreply.github.com>
2024-10-11 19:56:49 +0300

    loops and conditions + legacy

--- first-angular-app/src/app/app.component.ts
+++ first-angular-app/src/app/app.component.ts
@@ -3,19 +3,19 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy_users';
 import { TaskComponent } from './task/task.component';
-
+import { NgFor, NgIf } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TaskComponent],
+  imports: [HeaderComponent, UserComponent, TaskComponent, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'first-angular-app';
   users = DUMMY_USERS;
-  selectedUser = signal<string>('Select a user');
+  selectedUser?: string;
   onSelect(id: string) {
-    this.selectedUser.set(this.users.find((u) => u.id === id)!.name);
+    this.selectedUser = this.users.find((u) => u.id === id)?.name;
   }
 }
