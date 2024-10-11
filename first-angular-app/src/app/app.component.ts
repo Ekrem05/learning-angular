99a8375b530828c3d59937358092f2b66504d573
Ekrem Beytula <78847462+Ekrem05@users.noreply.github.com>
2024-10-11 19:30:37 +0300

    tasks component exercise

--- first-angular-app/src/app/app.component.ts
+++ first-angular-app/src/app/app.component.ts
@@ -1,20 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy_users';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'first-angular-app';
   users = DUMMY_USERS;
-
+  selectedUser = signal<string>('Select a user');
   onSelect(id: string) {
-    console.log(id);
+    this.selectedUser.set(this.users.find((u) => u.id === id)!.name);
   }
 }
