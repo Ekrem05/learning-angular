ae5b1856028eb7b865a64e620d5c3f0bf4b479d3
Ekrem Beytula <78847462+Ekrem05@users.noreply.github.com>
2024-10-17 20:54:57 +0300

    temp converter pipe implementation

--- pipes/src/app/pipes/temperature.pipe.ts
+++ pipes/src/app/pipes/temperature.pipe.ts
@@ -5,7 +5,26 @@ import { Pipe, PipeTransform } from '@angular/core';
   standalone: true,
 })
 export class TemperaturePipe implements PipeTransform {
-  transform(value: any, ...args: any[]) {
-    return value + '-transformed';
+  transform(
+    value: string | number | null,
+    inputType: 'cel' | 'fah',
+    outputType: 'cel' | 'fah'
+  ) {
+    let val: number;
+    if (!value) {
+      return value;
+    }
+    if (typeof value == 'string') {
+      val = parseFloat(value);
+    } else {
+      val = value;
+    }
+    let output = val;
+    if (inputType === 'cel' && outputType == 'fah') {
+      output = val * (9 / 5) + 32;
+    } else if (inputType === 'fah' && outputType == 'cel') {
+      output = (val - 32) * (5 / 9);
+    }
+    return `${output.toFixed(2)} ${outputType == 'cel' ? 'C' : 'F'}`;
   }
 }
