f48cf3d07835e893430c8198db5dddedc151eea0
Ekrem Beytula <78847462+Ekrem05@users.noreply.github.com>
2024-10-17 19:46:21 +0300

    custom directive logic

--- directives/src/app/directives/save-link.directive.ts
+++ directives/src/app/directives/save-link.directive.ts
@@ -3,9 +3,23 @@ import { Directive } from '@angular/core';
 @Directive({
   selector: 'a[safeLink]',
   standalone: true,
+  host: {
+    '(click)': 'onConfirmLeavePage($event)',
+  },
 })
 export class SafeLinkDirective {
   constructor() {
     console.log('active');
+  }
+
+  onConfirmLeavePage(event: MouseEvent) {
+    const wantsToLeave = window.confirm(
+      'Are you sure you want to leave the app?'
+    );
+
+    if (wantsToLeave) {
+      return;
+    }
+    event.preventDefault();
   }
 }
