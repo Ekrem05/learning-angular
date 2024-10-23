6f8ce4cc0d685b9955719ed474a47ef13ba1cfda
Ekrem Beytula <78847462+Ekrem05@users.noreply.github.com>
2024-10-23 19:11:54 +0300

    init

---
+++ rxjs-observables/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+})
+export class AppComponent implements OnInit {
+  ngOnInit(): void {}
+}
