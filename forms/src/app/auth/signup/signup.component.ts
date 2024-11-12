0670ef27e226d8f8b4c4b658d9e3cb945606f20f
Ekrem Beytula <78847462+Ekrem05@users.noreply.github.com>
2024-11-12 18:26:51 +0200

    Reactive forms exercise

--- forms/src/app/auth/signup/signup.component.ts
+++ forms/src/app/auth/signup/signup.component.ts
@@ -1,9 +1,57 @@
 import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
-export class SignupComponent {}
+export class SignupComponent {
+  form = new FormGroup(
+    {
+      email: new FormControl('', {
+        validators: [Validators.email],
+      }),
+      password: new FormControl('', {
+        validators: [Validators.maxLength(10), Validators.minLength(6)],
+      }),
+      confirmPassword: new FormControl('', {}),
+    },
+    {
+      validators: (control: AbstractControl) => {
+        if (
+          control.get('password')?.value ===
+          control.get('confirmPassword')?.value
+        ) {
+          return null;
+        }
+        return { doesNotMatchPassword: false };
+      },
+    }
+  );
+
+  onSubmit() {
+    console.log(this.form);
+  }
+  get isEmailValid() {
+    if (this.form.controls.email.valid) {
+      return true;
+    }
+    return false;
+  }
+  get doesPasswordMatch() {
+    if (this.form.errors && this.form.controls.confirmPassword.touched) {
+      return false;
+    }
+    return true;
+  }
+}
